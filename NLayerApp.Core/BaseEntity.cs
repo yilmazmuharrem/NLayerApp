29e53e8e60fe14639b9f3c750f212f4424fa5642
yilmazmuharrem <muharremyilmaz656@gmail.com>
2024-01-05 11:20:05 +0300

    Add project files.

---
+++ NLayerApp.Core/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace NLayerApp.Core
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
---
+++ NLayerApp.Core/Product.cs
@@ -0,0 +1,18 @@
+﻿
+namespace NLayerApp.Core
+{
+    public class Product
+    {
+        public string? Name { get; set; }
+
+        public int Stock { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public Category? Category { get; set; }
+
+        public ProductFeature? ProductFeature { get; set; }
+    }
+}
