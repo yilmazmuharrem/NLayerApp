fbecd55c996dbc596261f59b705f9dc6d3d957e4
yilmazmuharrem <muharremyilmaz656@gmail.com>
2024-01-05 11:39:40 +0300

    Ders 19. Service eklenmesi

--- NLayerApp.Core/Repositories/IGenericRepository.cs
+++ NLayerApp.Core/Repositories/IGenericRepository.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System.Linq.Expressions;
+
 
 namespace NLayerApp.Core.Repositories
 {
-    internal interface IGenericRepository
+    public interface IGenericRepository<T> where T : class
     {
+        Task<T> GetByIdAsync(int id,CancellationToken cancellationToken);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);
+        IQueryable<T> Where(Expression<Func<T,bool>> expression);
+        bool AnyAsync(Expression<Func<T, bool>> expression,CancellationToken cancellationToken);
+        Task AddAsync(T entity,CancellationToken cancellationToken);
+        Task AddRangeAsync(IEnumerable<T> entities,CancellationToken cancellationToken);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
     }
 }
---
+++ NLayerApp.Core/Services/IService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.Core.Services
+{
+    public interface IService<T> where T : class
+    {
+
+        Task<T> GetByIdAsync(int id, CancellationToken cancellationToken);
+        Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken);
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        bool AnyAsync(Expression<Func<T, bool>> expression, CancellationToken cancellationToken);
+        Task AddAsync(T entity, CancellationToken cancellationToken);
+        Task AddRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken);
+        Task UpdateAsync(T entity,CancellationToken cancellationToken);
+        Task RemoveAsync(T entity, CancellationToken cancellationToken);
+        Task RemoveRangeAsync(IEnumerable<T> entities, CancellationToken cancellationToken);
+    }
+}
